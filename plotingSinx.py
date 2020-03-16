ee25ae9e78f6f604ba585597411803ef9a3dd979
Hafiz Mughees Ahmad <itsmughees@gmail.com>
2020-03-16 23:51:37 +0300

    Update plotingSinx.py

--- plotingSinx.py
+++ plotingSinx.py
@@ -1,7 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+ 
 x = np.linspace(0, 10, 100)
-
-fig = plt.figure()
-plt.plot(x, np.sin(x), '-')
-plt.plot(x, np.cos(x), '--');
+ 
+plt.plot(x, np.sin(x))
+plt.plot(x, np.cos(x))
+ 
+plt.show()
